759c963bb433b22a6587997f1f5ca3023e106860
Leyan AlAjmi <lmnalajmi1@sheffield.ac.uk>
2023-04-30 20:16:17 +0300

    UI

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,20 @@ var userController = require('../controller/userController')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+    res.render('index', {title: 'Bird Watching'});
 });
 router.get('/detail', function(req, res, next) {
   res.render('detail', { title: 'Sighting Detail' });
 });
+router.get('/bird-details', function(req, res, next) {
+  res.render('bird-details', { title: 'Bird Watching' });
+});
+router.get('/bird-details-tabular', function(req, res, next) {
+  res.render('bird-details-tabular', { title: 'Bird Watching' });
+});
 router.post('/setUser', function (req,res){
   userController.createUserInMongo(req, res).then(r =>{
-
-
+})
 })
 
 
