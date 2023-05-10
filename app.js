4b3d6398c00b29a6cab4bc2bc412c5c1ae3ab796
Pradeep Detroja <pkdetroja1@sheffield.ac.uk>
2023-05-10 12:31:37 +0100

    comments: -Used const instead of var for variable declarations. -Used arrow functions instead of function expressions for the middleware functions

--- app.js
+++ app.js
@@ -1,41 +1,50 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
-// view engine setup
+// Configure view engine and views directory
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Middleware for logging
 app.use(logger('dev'));
+
+// Middleware for parsing JSON and urlencoded request bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Middleware for parsing cookies
 app.use(cookieParser());
+
+// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Mount routers for handling requests
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Middleware for handling 404 errors
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Middleware for handling errors
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals for rendering error page
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render error page
   res.status(err.status || 500);
   res.render('error');
 });
 
+// Export app for use in other modules
 module.exports = app;
