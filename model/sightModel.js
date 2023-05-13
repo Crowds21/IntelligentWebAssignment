77dac387251a8eeb50f20d6a155c6b0c0eeb4091
Pradeep Detroja <pkdetroja1@sheffield.ac.uk>
2023-05-13 14:28:58 +0100

    sightModel

--- model/sightModel.js
+++ model/sightModel.js
@@ -1,5 +1,17 @@
 const mongoose = require("mongoose");
 
+/**
+ * @property {string} identification - The identification of the sight.
+ * @property {string} description - The description of the sight.
+ * @property {string} date - The date of the sight.
+ * @property {string} user_name - The username of the user who added the sight.
+ * @property {string} location - The location of the sight.
+ * @property {Object} loc - The location coordinates of the sight.
+ * @property {string} loc.lat - The latitude of the location coordinates of the sight.
+ * @property {string} loc.lng - The longitude of the location coordinates of the sight.
+ * @property {string} image - The image of the sight.
+ */
+
 const SightSchema = mongoose.Schema({
     identification: {type: String, required: true},
     description: {type: String, required: true},
@@ -13,5 +25,6 @@ const SightSchema = mongoose.Schema({
     },
     image: {type: String, required: false}
 });
+
 SightSchema.set('toObject', {getters: true});
 module.exports = mongoose.model("sight", SightSchema, "sights");
