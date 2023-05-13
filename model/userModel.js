fa7c25a0c000633d96e5871c71d20f36e9e170a6
Pradeep Detroja <pkdetroja1@sheffield.ac.uk>
2023-05-13 14:32:50 +0100

    userModel

--- model/userModel.js
+++ model/userModel.js
@@ -1,6 +1,15 @@
 let mongoose = require('mongoose')
 let Schema = mongoose.Schema;
 
+/**
+ * Create a new mongoose model instance for the user collection.
+ *
+ * @function
+ * @param {string} name - The name for the model.
+ * @param {UserSchema} schema - The user schema object.
+ * @param {string} collection - The name of the MongoDB collection to use for the model.
+ * @returns {User} The mongoose model for user collection.
+ */
 let UserSchema = new Schema(
     {
         user_id: {type: String, require: true, max: 50},
